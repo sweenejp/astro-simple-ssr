4e5d75f5474c6a19bfb5055f2c8e340d6030b722
Jimmy Sweeney <sweenejp@gmail.com>
2024-03-20 21:53:40 -0500

    middleware mode instead of standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
+    mode: 'middleware',
+  }),
 });
